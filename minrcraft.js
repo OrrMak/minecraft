a30daabc95f92f4b7c0d0e7dc163fd374c15a715
OrrMak <makonnenrocks@gmail.com>
2024-10-03 20:37:57 +0300

    2dm array board

--- minrcraft.js
+++ minrcraft.js
@@ -1,3 +1,21 @@
 
-const skyTiles=document.getElementsByClassName('right');
-console.log(skyTiles);
+const gameBoard=document.getElementsByClassName('right')[0];
+// console.log(skyTiles);
+
+//make the gameBoard as 2d array, create background tiles.
+for(let i=1; i<6; i++){
+    const row=document.createElement('div');
+    row.classList.add('row'); 
+    const boardArray=[];
+        
+        for(let j=1; j<9; j++){
+            const rowTile=document.createElement('div');
+            rowTile.setAttribute('id', `sky-${i}-${j}`);
+            row.appendChild(rowTile);
+            
+            boardArray.push(rowTile);
+        }
+    boardArray.push(row);
+    gameBoard.appendChild(row);
+
+}
