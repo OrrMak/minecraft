850745d5cd2ffbd9ff850f59be784932128a66db
OrrMak <makonnenrocks@gmail.com>
2024-10-04 23:15:17 +0300

    trees

--- mc2.js
+++ mc2.js
@@ -1,18 +1,17 @@
 
-
 const gameBoard=document.getElementsByClassName('right')[0];
 const boardArray=[];
 let selectedTool=null;
+
 //menu 
 const menu=document.getElementsByClassName('left')[0];
 console.log(menu);
-const toolsArray=['Axe','Pickaxe','Shovel','rockPlacer'];
+const toolsArray=['Axe','Pickaxe','Shovel','rockPlacer','grassPlacer','soilPlacer'];
 
 //create tools:
 for (i=0; i<toolsArray.length; i++){
     const tool=document.createElement('div');
     tool.setAttribute('id',toolsArray[i]);
-    // tool.textContent=toolsArray[i];
 
      //click events for tools, only one tool can be clicked at a time.
      tool.addEventListener('click', function() {
@@ -23,9 +22,7 @@ for (i=0; i<toolsArray.length; i++){
         tool.classList.add('clicked');
             console.log(`tool clicked: ${tool.id}`); // Log the clicked tile ID
             selectedTool=tool.id;
-            // tool.setAttribute('class','CurrentTool');
     });
-    // console.log(selectedTool);
     menu.appendChild(tool);
 }
 
@@ -36,7 +33,6 @@ Axe.innerText ='axe';
 const rockCounter=document.getElementById('rockPlacer');
 rockCounter.innerText ='0';
 let currentRockCount=parseInt(rockPlacer.innerText);
-// console.log(currentRockCount);
 
 //Create background tiles and store them in a 2D array
 for(let i=1; i<16; i++){
@@ -72,7 +68,9 @@ for(let i=1; i<16; i++){
                     currentRockCount-=1;
                     rockCounter.innerText=currentRockCount;
             } }
-        });
+            });
+
+
             row.appendChild(rowTile);
             boardArray.push(rowTile);
         }
@@ -92,23 +90,44 @@ function rock(){
 }
 rock();
 
-console.log(currentRockCount);
+//function the creates soil:
+function soil(){
+    for(let i=12; i<=13; i++){
+        for(let j=1; j<16; j++){
+            const soil=document.getElementById(`tile-${i}-${j}`)
+            soil.classList.add('soil');
+        }
+    }
+}
+soil();
+//function the creates grass:
+function grass(){
+    for(let i=11; i<=11; i++){
+        for(let j=1; j<16; j++){
+            const grass=document.getElementById(`tile-${i}-${j}`)
+            grass.classList.add('grass');
+        }
+    }
+}
+grass();
+//function the creates treetrunk:
+function treeTrunk(jNum){
+    for(let i=6; i<=10; i++){
+        for(let j=jNum; j<=jNum; j++){
+            const treeTrunk=document.getElementById(`tile-${i}-${j}`)
+            treeTrunk.classList.add('treeTrunk');
+        }
+    }
+    //function the creates treetrunk:
+    // function leaves(lNum){
+        for(let i=2; i<=5; i++){
+            for(let j=jNum-2; j<=jNum+2; j++){
+                const leaves=document.getElementById(`tile-${i}-${j}`)
+                leaves.classList.add('leaves');
+            }   
+        }  
+}
 
-
-
-
-//when rock counter clicks -- adds rock to tiles in the board game
-// rockCounter.addEventListener('click', function() {
-    // rockCounter.classList.add('clicked');
-//     currentTool.classList.remove('clicked
-// ')
-
-//     if(rockCounter.classList.contains('clicked') && currentRockCount>0){
-//         // && (!rowTile.classList.contains('rock'))
-//         rowTile.classList.add('rock');
-//         console.log(`added rock to:, ${tileID}`);
-//         currentRockCount-=1;
-//         rockCounter.innerText=currentRockCount;
-//    }
-
-
+treeTrunk(4);
+treeTrunk(10);
+ 
