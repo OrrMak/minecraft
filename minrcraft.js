9d271269d205425120081d65fe50f00ca2daa068
OrrMak <makonnenrocks@gmail.com>
2024-10-04 18:31:30 +0300

    before refactor

--- minrcraft.js
+++ minrcraft.js
@@ -13,13 +13,18 @@ for(let i=1; i<16; i++){
             rowTile.setAttribute('class','boardTile');
             const tileID=rowTile.id;
 
-            //click events
+            //add click events
             rowTile.addEventListener('click', function() {
                console.log(`Tile clicked: ${tileID}`); // Log the clicked tile ID
-               
+            
+            //rock   
                if(rowTile.classList.contains('rock') && selectedTool==='Axe'){
                     rowTile.classList.remove('rock');
                     console.log(`remove the rock from:, ${tileID}`);
+                    
+                    currentRockCount+=1;
+                    rockCounter.innerText=currentRockCount;
+                    rockCounter.setAttribute('id','rockCounter');
                }
             });
 
@@ -30,7 +35,7 @@ for(let i=1; i<16; i++){
     gameBoard.appendChild(row);
 }
 
-//function the creates cement:
+//function the creates rock:
 function rock(){
     for(let i=14; i<=15; i++){
         for(let j=1; j<16; j++){
@@ -54,14 +59,16 @@ const menu=document.getElementsByClassName('left')[0];
 console.log(menu);
 const toolsArray=['Axe','Pickaxe','Shovel'];
 let selectedTool;
+
 //create tools:
 for (i=0; i<toolsArray.length; i++){
     const tool=document.createElement('div');
     tool.setAttribute('id',toolsArray[i]);
+    tool.setAttribute('class','CurrentTool');
     tool.textContent=toolsArray[i];
 
      //click events
-    tool.addEventListener('click', function() {
+     tool.addEventListener('click', function() {
         const currentClicked=document.querySelector('.left .clicked');
         if (currentClicked){
             currentClicked.classList.remove('clicked')
@@ -71,10 +78,30 @@ for (i=0; i<toolsArray.length; i++){
             console.log(`tool clicked: ${tool.id}`); // Log the clicked tile ID
             selectedTool=tool.id;
     });
-
     console.log(tool);
     menu.appendChild(tool);
 }
 
+const currentTool=document.querySelector('.currentTool');
+console.log(`currentTool: ${currentTool}`)
+
 //create rock counter
+const rockCounter=document.createElement('div');
+menu.appendChild(rockCounter);
+rockCounter.innerText ='0';
+let currentRockCount=parseInt(rockCounter.innerText);
+
+//when rock counter clicks -- adds rock to tiles in the board game
+rockCounter.addEventListener('click', function() {
+    rockCounter.classList.add('clicked');
+    currentTool.classList.remove('clicked')
+
+    if(rockCounter.classList.contains('clicked') && currentRockCount>0){
+        // && (!rowTile.classList.contains('rock'))
+        rowTile.classList.add('rock');
+        console.log(`added rock to:, ${tileID}`);
+        currentRockCount-=1;
+        rockCounter.innerText=currentRockCount;
+   }
+});
 
